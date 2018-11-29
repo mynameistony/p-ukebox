f9c546810ff4f1b878b78cde8b08497be9b402b7
P-ukebox <tonyrogers10195@gmail.com>
2018-11-29 15:24:55 -0800

    Added backend skeletor

---
+++ backend/actions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#echo "Bash: $@"
+
+case $@ in
+	"togglePlay")
+		echo "add to backend"
+	;;
+
+	"previousSong")	
+		echo "add to backend"
+	;;
+
+	"nextSong")
+		echo "add to backend"
+	;;
+
+	"showArtists")
+	echo "add to backend";;
+
+	"showTracks")
+	echo "add to backend";;
+
+	"showGenres")
+	echo "add to backend";;
+esac
