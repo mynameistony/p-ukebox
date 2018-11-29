f09574e2b6aba3d09e4f990ee1152341c4cb6e4b
Tony <tonyrogers10195@gmail.com>
2018-11-28 17:41:05 -0800

    Welcome to the p-ukebox

---
+++ index.php
@@ -0,0 +1,13 @@
+<script> src="/scripts.js"</script>
+<link href=/style.css rel="stylesheet" type="text/css">
+<body>
+	<button class=button> fuck </button>
+	<p></p>
+	<button class="half button"> fuck </button>
+	<button class="half button"> fuck </button>
+	<p></p>
+	<button class="third button"> fuck </button>
+	<button class="third button"> fuck </button>
+	<button class="third button"> fuck </button>
+<?php ?>
+</body>
