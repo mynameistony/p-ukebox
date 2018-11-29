6cff2ab9f67c26e9bff970a9798ec54e045de020
P-ukebox <tonyrogers10195@gmail.com>
2018-11-29 13:52:12 -0800

    Front end baseline functional

---
+++ webpage/scripts.js
@@ -0,0 +1,43 @@
+function buttonPressed(thisButton){
+	alert(thisButton);
+
+	switch(thisButton){
+		case "togglePlay":
+
+		break;
+
+		case "previousSong":
+
+		break;
+
+		case "nextSong":
+
+		break;
+
+		case "showArtists":
+
+		break;
+
+		case "showTracks":
+
+		break;
+
+		case "showGenres":
+
+		break;
+
+		default:
+			alert("Ya dun goofd!");
+		break;
+	}
+};
+
+function sendAction(thisAction, thisResult){
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function(){
+		if(request.readyState == 4 && request.status == 200)
+			buttonPressed(request.responseText);
+	}
+	request.open("GET", "/actions.php?action="+thisAction, true);
+	request.send(null);
+}
